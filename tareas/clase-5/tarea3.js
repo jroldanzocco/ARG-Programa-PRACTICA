a926e4e881d8e9f5fedd3410f32b73b86ef57de6
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2022-01-04 01:16:53 -0300

    Resuelto tarea3
    Se agrego la funcion Math.trunc() que permitio devolver los numeros sin decimales

---
+++ tareas/clase-5/tarea3.js
@@ -0,0 +1,46 @@
+let $botonCalcular = document.querySelector("#calcular-duracion");
+
+$botonCalcular.onclick = function () {
+  let segundosClases = document.querySelectorAll(".segundo");
+  let minutosClases = document.querySelectorAll(".minuto");
+  let horasClases = document.querySelectorAll(".hora");
+  const segundosPorMinuto = 60;
+  const minutosPorHora = 60;
+
+  segundosClases = calcularTotalTiempo(segundosClases);
+  minutosClases = calcularTotalTiempo(minutosClases);
+  horasClases = calcularTotalTiempo(horasClases);
+
+  let minutosExcedentes = 0;
+  let horasExcedentes = 0;
+
+  if (segundosClases >= segundosPorMinuto) {
+    minutosExcedentes = Math.trunc(convertirSegundoAMinuto(segundosClases));
+    minutosClases = minutosExcedentes + minutosClases;
+    segundosClases = segundosClases - minutosExcedentes * segundosPorMinuto;
+  } else if (minutosClases >= minutosPorHora) {
+    horasExcedentes = Math.trunc(convertirMinutoAHora(minutosClases));
+    horasClases = horasExcedentes + horasClases;
+    minutosClases = minutosClases - minutosExcedentes * minutosPorHora;
+  }
+
+  document.querySelector(
+    "#duracion-total"
+  ).innerText = `${horasClases} horas ${minutosClases} minutos ${segundosClases} segundos`;
+};
+
+function convertirSegundoAMinuto(segundos) {
+  return segundos / 60;
+}
+
+function convertirMinutoAHora(minutos) {
+  return minutos / 60;
+}
+
+function calcularTotalTiempo(nodo) {
+  let totalTiempo = 0;
+  for (let i = 0; i < nodo.length; i++) {
+    totalTiempo += Number(nodo[i].value);
+  }
+  return totalTiempo;
+}
