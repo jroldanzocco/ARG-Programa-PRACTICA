db0e0b2726d2e4786d0a0e91669ac23d84d8634a
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2022-01-05 04:52:57 -0300

    Calculo del promedio
    Se realizo el calculo del promedio de numeros y se inserto en una etiqueta em creada con un texto predefinido

--- tareas/clase-5/tarea4.js
+++ tareas/clase-5/tarea4.js
@@ -2,9 +2,21 @@ let listadeNumeros = document.querySelectorAll("li");
 let arrayGenerado = convertirListaEnArray(listadeNumeros);
 
 function convertirListaEnArray(lista) {
-  let = arrayGenerado = [];
+  let arrayGenerado = [];
   for (let i = 0; i < lista.length; i++) {
     arrayGenerado.push(Number(lista[i].innerText));
   }
   return arrayGenerado;
+}
+
+let promedioArray = promediarNumerosArray(arrayGenerado);
+let $promedioLista = document.querySelector("#promedio-lista");
+$promedioLista.insertAdjacentHTML("beforeend", promedioArray);
+
+function promediarNumerosArray(array) {
+  let suma = 0;
+  for (let i = 0; i < array.length; i++) {
+    suma += array[i];
+  }
+  return suma / array.length;
 }
