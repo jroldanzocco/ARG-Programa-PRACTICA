1172f7936f25e5dd7faf0e5ede62f1460dbbe91a
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2022-01-14 06:42:30 -0300

    Solo ejecutar una vez
    Se modifico el boton de  ingresar integrantes para que solo se pueda ingresar valores una vez

--- tareas/clase-6/tarea1.js
+++ tareas/clase-6/tarea1.js
@@ -1,3 +1,21 @@
+// Primer botón
+$botonEnviarIntegrantes = document.querySelector("#envio");
+$botonEnviarIntegrantes.onclick = function () {
+  const numeroDeIntegrantes = Number(
+    document.querySelector("#cantidad-integrantes").value
+  );
+
+  if (numeroDeIntegrantes <= 0) {
+    return false;
+  } else {
+    agregarMasMiembros(numeroDeIntegrantes);
+  }
+
+  document.querySelector("#envio").onclick = null;
+};
+
+//Creacion de un nuevo integrante
+
 function crearNuevoMiembro(cantidad) {
   const cantidadIntegrantes = Number(
     document.querySelector("#cantidad-integrantes").value
@@ -8,44 +26,13 @@ function crearNuevoMiembro(cantidad) {
   nuevoLabel.textContent = "Ingrese la edad del familiar N°" + cantidad + ": ";
   let nuevoInput = document.createElement("input");
   nuevoInput.type = "number";
-
   nodoForm.appendChild(nuevoDiv);
   nuevoDiv.appendChild(nuevoLabel);
   nuevoLabel.appendChild(nuevoInput);
 }
-
+//bucle que permite crear la cantidad indicada de miembros
 function agregarMasMiembros(numeroDeIntegrantes) {
   for (let i = 1; i <= numeroDeIntegrantes; i++) {
     crearNuevoMiembro(i);
   }
 }
-
-$botonEnviarIntegrantes = document.querySelector("#envio");
-
-$botonEnviarIntegrantes.onclick = function () {
-  const numeroDeIntegrantes = Number(
-    document.querySelector("#cantidad-integrantes").value
-  );
-  if (numeroDeIntegrantes <= 0) {
-    return false;
-  } else {
-    agregarMasMiembros(numeroDeIntegrantes);
-  }
-};
-
-/*
-let $nuevoForm;
-let $nuevoInput;
-
-function agregarMiembroFamiliar() {
-  $nuevoForm = document.createElement("form");
-  $nuevoForm.className = "integrantes";
-
-  const $nuevoLabel = document.createElement("label");
-  $nuevoLabel.innerText = "Ingresa la edad del familiar";
-  $nuevoInput = document.createElement("input");
-  $nuevoInput.type = "number";
-  $nuevoInput.className = "edad";
-}
-
-*/
